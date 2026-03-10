4aee8f5fb91fc9fa087f81b98d0f7fcc3884b387
N-Yashwitha <nettemyashwitha1@gmail.com>
2026-03-10 08:48:43 +0530

    FSAD skill exam project

---
+++ department-crud/src/main/java/com/klef/fsad/exam/department_crud/App.java
@@ -0,0 +1,13 @@
+package com.klef.fsad.exam.department_crud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
